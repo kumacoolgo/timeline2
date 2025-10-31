60aa396ff00af2bc250a6d72cba4d6573acfecb0
kumacoolgo <okchenyuejie@gmail.com>
2025-10-31 10:03:31 +0900

    Fix: all

--- api/logout.js
+++
@@ -1,9 +0,0 @@
-
-const { json, destroySession, ensureCsrfCookie, requireCsrf } = require('./_utils');
-module.exports = async (req, res) => {
-  ensureCsrfCookie(req, res);
-  if(req.method!=='POST') return json(res,{error:'Method Not Allowed'},405);
-  if (!requireCsrf(req)) return json(res, { error: 'CSRF 验证失败' }, 403);
-  await destroySession(req,res);
-  return json(res,{ok:true});
-};
